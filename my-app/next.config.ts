fa0ea0d6967465a01bc8d73a8eaf3b98b3e85a08
Bada Jung <57929751+badasea@users.noreply.github.com>
2026-01-07 16:31:57 +0900

    fix: next.config basePath 수정

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
 const nextConfig: NextConfig = {
   /* config options here */
   // 빌드 시 정적 파일(HTML, CSS, JS)을 생성하도록 설정합니다.
@@ -9,7 +7,7 @@ const nextConfig: NextConfig = {
 
   // GitHub Pages 배포를 위한 경로 설정입니다.
   // '/저장소-이름' 형식으로 꼭 바꿔주세요!
-  basePath: basePath,
+  basePath: '/flower_achive',
 
   // next/image 사용 시 필요할 수 있는 설정입니다.
   images: {

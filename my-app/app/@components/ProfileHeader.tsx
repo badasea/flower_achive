a669afbf358ffb5e44a8f8da8b4f73d2d82b7ed6
jungbada269 <57929751+badasea@users.noreply.github.com>
2026-01-24 17:19:50 +0900

    feat: 이미지 추가 진행 중

--- my-app/app/@components/ProfileHeader.tsx
+++ my-app/app/@components/ProfileHeader.tsx
@@ -1,26 +1,30 @@
 // components/ProfileHeader.tsx
-'use client';
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
 
 export default function ProfileHeader() {
   return (
     <header className="flex gap-10 flex-row items-center p-4 md:p-8 max-w-6xl mx-auto">
       <div className="w-24 h-24 md:w-48 md:h-48 flex-shrink-0 mb-4 md:mb-0 md:mr-8">
         <Image
-          src="/flower_achive/next.svg"
+          src="/flower_achive/profile.png"
           alt="Profile"
           width={192}
           height={192}
           className="w-full h-full object-cover rounded-full border-4 border-green-500 p-1"
         />
       </div>
-      <div className="flex-1 min-w-0"> 
+      <div className="flex-1 min-w-0">
         <div className="flex flex-wrap items-center mb-4 gap-4">
           <h1 className="text-1xl font-bold">바다의 꽃꽂이 아카이브</h1>
         </div>
         <ul className="flex gap-8 mb-4 text-sm">
-          <li><span className="font-bold block text-lg">21</span> posts</li>
-          <li><span className="font-bold block text-lg">2025</span>since</li>
+          <li>
+            <span className="font-bold block text-lg">22</span> posts
+          </li>
+          <li>
+            <span className="font-bold block text-lg">2025</span>since
+          </li>
         </ul>
       </div>
     </header>
--- my-app/app/@constant/post.tsx
+++ my-app/app/@constant/post.tsx
@@ -1,26 +1,26 @@
 export const posts = [
-    {
-        id: 0,
-        src: "/flower_achive/vercel.svg",
-        likes: 1,
-        comments: 1
-    },
-    {
-        id: 1,
-        src: "/flower_achive/window.svg",
-        likes: 1,
-        comments: 1
-    },
-    {
-        id: 2,
-        src: "/flower_achive/file.svg",
-        likes: 1,
-        comments: 1
-    },
-    {
-        id: 3,
-        src: "/flower_achive/globe.svg",
-        likes: 1,
-        comments: 1
-    }
+  {
+    id: 0,
+    src: "/flower_achive/post_3.png",
+    likes: 1,
+    comments: 1,
+  },
+  {
+    id: 1,
+    src: "/flower_achive/post_2.png",
+    likes: 1,
+    comments: 1,
+  },
+  {
+    id: 2,
+    src: "/flower_achive/post_1.png",
+    likes: 1,
+    comments: 1,
+  },
+  {
+    id: 3,
+    src: "/flower_achive/post_0.png",
+    likes: 1,
+    comments: 1,
+  },
 ];
